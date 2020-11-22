fb8d89f1fe12ea846ba1a5ae9fabb24656781dad
Serge <s@shayderov.ru>
2020-11-22 13:02:32 +0300

    Form fields trask using vue cli

---
+++ task4-1/src/components/Input.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="form-group">
+    <label>{{ field.title }}</label>
+    <span v-show="'valid' in field"
+          class="fas"
+          :class="field.valid ? 'fa-check' : 'fa-exclamation'"></i>
+    </span>
+    <input type="text"
+           class="form-control"
+           v-model.lazy="field.value"
+           @change="$emit('field-update')">
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      field: Object
+    }
+  }
+</script>
+
+<style scoped>
+  .fa-exclamation {
+    color: red;
+  }
+  .fa-check {
+    color: green;
+  }
+</style>
