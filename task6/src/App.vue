d3173c228f9ae28f23c637a32c43e5b877b0c4e2
Serge <s@shayderov.ru>
2020-11-29 20:05:52 +0300

    Move form and table into components

--- task6/src/App.vue
+++ task6/src/App.vue
@@ -3,92 +3,31 @@
     id="app"
     class="container"
   >
-      <form
-        @submit.prevent="submitForm"
-        v-if="!submitted"
-      >
-        <div class="progress">
-          <div
-            class="progress-bar"
-            role="progressbar"
-            aria-valuemin="0"
-            aria-valuemax="100"
-            :aria-valuenow="currentProgress"
-            :style="currentStyle"
-          >
-          </div>
-        </div>
-        <app-input
-          v-for="(_, index) in fields"
-          :key="index"
-          :index="index"
-        >
-        </app-input>
-        
-        <button class="btn btn-primary">Send Data</button>
-      </form>
-      <div v-else>
-        <h2>Hello, {{ name }}</h2>
-        <hr>
-        <table class="table table-bordered">
-          <tbody>
-            <tr
-              v-for="(field, index) in fields"
-              :key="index"
-            >
-              <td>{{ field.title }}</td>
-              <td>{{ field.value }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+    <form-progress
+      v-if="!submitted"
+      @form-submit="submitted = true"
+    ></form-progress>
+    <table-submitted v-else></table-submitted>
   </div>
 </template>
 
 <script>
-  import AppInput from './components/InputTextValidation';
-  import { mapGetters } from 'vuex';
+  import FormProgress from './components/FormProgress';
+  import TableSubmitted from './components/TableSubmitted';
 
   export default {
     name: 'app',
     components: {
-      AppInput
+      FormProgress,
+      TableSubmitted
     },
     data () {
       return {
-        submitted: false,
-        fieldsTotal: 100
-      }
-    },
-    methods: {
-      submitForm() {
-        if (this.currentProgress === this.fieldsTotal) {
-          this.submitted = true;
-        }
-      }
-    },
-    computed: {
-      ...mapGetters([
-        'fields',
-        'name'
-      ]),
-      currentProgress() {
-        return this.fields.reduce((total, current) => {
-          if (current.valid) {
-            total += this.fieldProgress;
-          }
-          return total;
-        }, 0);
-      },
-      currentStyle() {
-        return `width: ${this.currentProgress}%;`;
-      },
-      fieldProgress() {
-        return Math.floor(this.fieldsTotal / this.fields.length)
+        submitted: false
       }
     }
   };
 </script>
 
-<style>
+<style scoped>
 </style>
---
+++ task6/src/components/TableSubmitted.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Hello, {{ name }}</h2>
+    <hr>
+    <table class="table table-bordered">
+      <tbody>
+        <tr v-for="(field, index) in fields" :key="index">
+          <td>{{ field.title }}</td>
+          <td>{{ field.value }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters([
+      'fields',
+      'name'
+    ])
+  }
+}
+</script>
+
+<style scoped>
+</style>
