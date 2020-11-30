8b99c8eb4cec9773f457f32c72c82ecea7ec3bd5
Serge <s@shayderov.ru>
2020-11-30 21:09:20 +0300

    Add router

--- task7/src/main.js
+++ task7/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue';
 import VueRouter from 'vue-router';
 
 import { store } from './store';
+import router from './router'
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App),
+  router,
   store
 }).$mount('#app');
